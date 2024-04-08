e0ca92a10c6e84e97a000e8ff8a53667d39ef12f
mhmunir <mhmunir914794@gmail.com>
2024-04-08 23:20:09 +0600

    added project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js}',
+  ],
+ 
+  theme: {
+    fontFamily: {
+      'Circular': ['Circular']
+    },
+    colors: {
+      white: '#fff',
+      lvory: '#F7F6F5',
+      charcoal: '#171717',
+      futureorange: '#FF5E3E',
+      sky_blue: '#8CB2C0',
+    },
+    
+  }
+}

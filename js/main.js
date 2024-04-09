bc3111c6357df5f436871786e6a2f475d225efb6
mhmunir <mhmunir914794@gmail.com>
2024-04-10 01:36:44 +0600

    nav style added

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ OpenNav.addEventListener('click',()=>{
 ClossNav.addEventListener('click',()=>{
     MobileNav.classList.remove('mobile-show');
 })
+
+
+var Sticky = "sticky";
+var scrollTrigger = 500;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("header")[0].classList.add(Sticky);
+  } else {
+    document.getElementsByTagName("header")[0].classList.remove(Sticky);
+  }
+};

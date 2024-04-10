47f100f68b4d47ad0e908aa3692cdf8ac3242ba0
mhmunir <mhmunir914794@gmail.com>
2024-04-11 00:00:20 +0600

    Submit page

--- js/form.js
+++ js/form.js
@@ -19,4 +19,42 @@ function sendEmail() {
   }).then(
     message => alert(message)
   );
+}
+
+
+
+function updateCSSClass(a, b) {
+}
+
+function checkfnlname(field) {
+  curr = document.getElementById(field).value;
+  if (curr.length > 0) {
+    updateCSSClass(field, 1);
+    return true;
+  } else {
+    updateCSSClass(field, 0);
+    return false;
   }
+}
+window.onload = function () {
+  var btnSubmit = document.getElementById('submit');
+  // disable submit
+  btnSubmit.setAttribute('disabled', 'disabled');
+
+  // attach the keyup event to each input
+  [].slice.call(document.querySelectorAll('form input:not([type="submit"])')).forEach(function (element, index) {
+    element.addEventListener('keyup', function (e) {
+      // compute the number of invalid fields
+      var invalidFields = [].slice.call(document.querySelectorAll('form input:not([type="submit"])')).filter(function (element, index) {
+        return !checkfnlname(element.id);
+      });
+      if (invalidFields.length == 0) {
+        // reenable the submit if n. of invalid inputs is 0
+        btnSubmit.removeAttribute('disabled');
+      } else {
+        // disable submit because there are invalid inputs
+        btnSubmit.setAttribute('disabled', 'disabled');
+      }
+    }, false);
+  });
+}
